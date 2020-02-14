0f933b6bfe3822f301612eb88c9032b589e3aff0
Lorran Sutter <sutterlorran@gmail.com>
2020-02-13 19:48:39 -0500

    Update controllers

--- Week02/local-library/app.js
+++ Week02/local-library/app.js
@@ -3,9 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var compression = require('compression');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog');
 
 //Import the mongoose module
 var mongoose = require('mongoose');
@@ -30,10 +32,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- Week02/local-library/routes/index.js
+++ Week02/local-library/routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Yay!' });
+router.get('/', function(req, res) {
+  res.redirect('/catalog');
 });
 
 router.get("/wow", function(req, res, next) {
